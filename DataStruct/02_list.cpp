f6634caf2f21d567067b036f582e0aa76b0039dd
fsxchen <fsxchen@gmail.com>
2014-12-01 17:12:10 +0800

    Cpp Stydy

--- DataStruct/02_list.cpp
+++ DataStruct/02_list.cpp
@@ -1,28 +1,44 @@
 #include <iostream>
 using namespace std;
 
-typedef int T;
 class List{
+    typedef int T;
     struct Node {
         T data;
         Node* next;
         Node(const T& d=T()):data(d), next(NULL){}  //零初始化,如果是基本类型，值为零，
-                                                    //如果是自建类型，则是建立对象，不传参数
+        //如果是自建类型，则是建立对象，不传参数
     };
     Node* head;                     //头指针，并不是一个节点，只是一个地址而已，
                                     //也可以使用一个头节点。带头节点的链表
+    int len;
 public:
-    List():head(NULL){    //构造方法，只是保存了第一个节点的地址
+    List():head(NULL),len(0){    //构造方法，只是保存了第一个节点的地址
     }
+    int size() const {
+        return len;
+    }
+    List& push_back(const T&d) {        //这里尽量使用引用，如果不是引用？？
+        insert(d, size());
+        return *this;
+    }
+    /*
+    List push_back(const T&d) {     如果不是引用，返回的时候，还会复制一份，但是这个
+    insert(d, size());              时候，由于head是一样的，当析构的时候，就会两次析构
+    return *this;
+    }
+    */
     void push_front(const T& d) {   //前插
         //Node p(d); 这样不行，因为出了这个范围d就释放了，需要使用动态内存
-        Node* p = new Node(d);
-        p->next = head;
-        head = p;
+        // Node* p = new Node(d);
+        // p->next = head;
+        // head = p;
+        // 有了insert的方法之后，可以这么写
+        insert(d, 0);
     }
     Node*& getptr(int pos){      //找到pos处的指针,？？为什么要用引用
                                         //因为直接带回去的就是指针本身，如果是指针就不是这样了
-                                        //如果返回指针p 需要p->next->next 才可以用
+                                        //如果返回指针p 需要p->next->next 才可以用,也就是要用二级指针
         if(pos == 0) return head;
         Node* p = head;
         for(int i = 1; i < pos; i++) {
@@ -32,11 +48,13 @@ public:
     }
     void insert(const T& d, int pos) {    //在pos插入
         //Node *p = getptr(pos)   p不是链表里面的指针
+        if (pos < 0 || pos > this->size()) pos = 0;
         Node*& pn = getptr(pos);      //依然是引用，不用引用，需要使用p->next->next
                                     //因为如果要修改，必须从链表里面的地址来修改
         Node* p = new Node(d);
         p->next = pn;
         pn = p;
+        len++;
     }
 
     void travel() const {  //遍历
@@ -55,8 +73,48 @@ public:
         }
     }
     ~List() {
+        cout << this << "************" << head << endl;
         clear();
     }
+    void erase(int pos){    //按照位置删除
+        if(pos < 0 || pos >= size()) return;   //有效位置判断
+        Node*& pn = getptr(pos);        //注意，要操作的还是链表里面的地址，否则还是要用二级指针
+        Node* p = pn;
+        pn = pn->next;
+        delete p;
+        this->len--;
+
+    }
+    void remove(const T& d) {    //按照数值来操作
+        int pos = -1;
+        while((pos = find(d)) != -1)
+            erase(pos);
+    }
+
+    int find(const T& d) const {
+        Node *p = head;
+        int pos = 0;
+        while(p) {
+            if(p->data == d) return pos;
+            p = p->next; ++ pos;
+        }
+        return -1;
+    }
+    void set(int pos, const T& d) {
+        if(pos < 0 || pos >= size()) return;
+        getptr(pos)->data = d;
+    }
+    bool empty() const {return head==NULL;}
+    T front() {if(empty()) throw "空"; return head->data;}
+    T end()const {
+        if(empty()) throw "空";
+        Node *p = head;
+        while(p->next != NULL){
+            p = p->next;
+        }
+        return p->data;
+    }
+
 };
 
 int main() {
@@ -65,7 +123,18 @@ int main() {
     l.push_front(8);
     l.push_front(20);
     l.insert(44, 1);
+    l.insert(4,10);
+    l.insert(10, 100);
+    l.insert(8, 3);
+    l.push_back(50).push_back(60);
     l.travel();
+    l.erase(1);
+    l.travel();
+    l.remove(8);
+    l.set(l.size()-1, 500);
+    l.set(l.find(5), 15);
+    l.travel();
+    cout << l.front() << "\t" << l.end() << '\t' << l.size() << endl;
     sizeof(l); //? =4/8其实就是一个地址
     return 0;
 }
