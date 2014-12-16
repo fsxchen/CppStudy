a5aaf357eaf1b819e7ffe87f2c2ab8987916ee1f
fsxchen <fsxchen@gmail.com>
2014-12-16 10:11:00 +0800

    fsxchen

--- CppSTL/07_sort.cpp
+++ CppSTL/07_sort.cpp
@@ -17,10 +17,33 @@ void sort(T* a, int n) {        //调用的时候，编译器可以推测
 
 struct Date{
     int y, m, d;
-    Date(int y, int m, int d):y(y), m(m), d(d) {}
+    // Date(int y, int m, int d):y(y), m(m), d(d) {}
 };
+
+bool operator<(const Date& a, const Date& b) {
+    if(a.y < b.y || a.y == b.y && (a.m < b.m || a.m == b.m && a.d < b.d))
+        return true;
+    else
+        return false;
+}
+
 ostream& operator<<(ostream& o, const Date&d) {
     return o << d.y << '-' << d.m << '-' << d.d;
+}
+
+template <class T>
+void show(T a[], int n){
+    for(int i = 0; i < n; i++)
+        cout << a[i] << ' ';
+    cout << endl;
+}
+
+template <typename T>
+void show(T& t) {
+    int n = sizeof(t)/ sizeof(t[0]);
+    for(int i = 0; i < n; i++)
+        cout << t[i] << ' ';
+    cout << endl;
 }
 
 int main() {
@@ -29,21 +52,16 @@ int main() {
     double d[4] = {1.2, 3.4, 8.9, 2.1};
     // sort(reinterpret_cast<int*>(d), 4);  强转。。不行哦！
     sort(d, 4);
-    Date x[3] = {{2010, 8, 30},
-                    {2014, 10, 21},
-                    {2014, 11, 23}};
 
-    for(int i = 0; i < 3; i++) {
-        cout << x[i] << ' ';
-    }
-    for(int i
-        = 0; i < 5; i++) {
-        cout << a[i] << ' ';
-    }
-    cout << endl;
-    for(int i = 0; i < 4; i++) {
-        cout << d[i] << ' ';
-    }
-    cout << endl;
+    Date x[3] = {{2010, 8, 30},{2014, 10, 21},{2014, 7, 23}};
+    sort(x, 3);
+
+    // show(x, 3);
+    // show(a, 5);
+    // show(d, 4);
+    //如何做到show(a);show(b)...
+    show(x);
+    show(a);
+    show(d);
     return 0;
 }
