05e93a0451a5696a330f61bf138539aef8c086cb
fsxchen <fsxchen@gmail.com>
2014-12-16 17:49:36 +0800

    fsxchen

--- CppSTL/08_Min.cpp
+++ CppSTL/08_Min.cpp
@@ -16,6 +16,16 @@ T Min(const T& a, const U& b) {
     cout << 'C'; return a < b ?a:T(b);
 }
 
+template <typename T>       //偏特化
+T Min(const T& a, char b) {
+    cout << 'E'; return a < b ?a:T(b);
+}
+
+template <>       //全特化,针对已经有的模板做的特化
+double& Min(double& a, double& b) {
+    cout << 'F'; return a < b ?a:b;
+}
+
 const char* Min(const char* a, const char* b) {     //如果普通函数和模板都可用的时候，编译器优先考虑普通函数
     cout << 'D'; return strcmp(a, b) < 0?a:b;
 }
@@ -29,4 +39,5 @@ int main() {
     cout << Min(5.9, 6) << endl;
     cout << Min("hello", "world") << endl;
     cout << Min<>("hello", "world") << endl;    //调用模板函数
+    cout << Min(123, 'x') << endl;
 }
