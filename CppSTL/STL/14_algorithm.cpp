21da79ba57072743c9f0486516bb075169a16ab2
fsxchen <fsxchen@gmail.com>
2014-12-26 09:27:30 +0800

    cpp

--- CppSTL/STL/14_algorithm.cpp
+++ CppSTL/STL/14_algorithm.cpp
@@ -1,17 +1,36 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-
-void add10(int& element) {
+#include <string>
+void add10(int& element)
+{
     element += 10;
 }
-
-void print(int& element) {
-    cout << element << endl;;
+void print(int element)
+{
+    cout << element << ' ';
 }
+class add{
+    int delta;
+public:
+    add(int d):delta(d){}
+    void operator()(int& element){      //重载括号运算符，构造函数对象类型的函数
+        element += delta;
+    }
+};
+template <typename Iter, typename Func>
 
-int main() {
-    int a[5] = {11, 22, 33, 44, 55};
-    for_each(a, a+5, add10);
-    for_each(a, a+5, print);
-}
+void foreach(Iter ib, Iter ie, Func f)
+{
+    while(ib!=ie) f(*ib++);
+    }
+    int main()
+    {
+        int a[5] = {11,22,33,44,55};
+        for_each(a,a+5,add10);
+        for_each(a,a+5,print);cout<<endl;
+        for_each(a,a+5,add(5));
+        for_each(a,a+5,print);cout<<endl;
+        for_each(a,a+5,add(1));
+        for_each(a,a+5,print);cout<<endl;
+    }
---
+++ CppSTL/STL/15_copy.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+#include "print.h"
+#include <algorithm>
+
+template <typename Iter, typename Pos>
+void co(Iter ib, Iter ie, Pos p) {
+    while(ib != ie) *p++ = *ib++;
+}
+
+int main() {
+    int a[5] = {3, 9, 2, 6, 8};
+    int b[8] = {0};
+    vector<int> vi(a, a+5);
+    sort(vi.begin(), vi.end());
+    print(vi.begin(), vi.end());
+    print(a, a+5, ',');
+    copy(vi.begin(), vi.end(), a);
+    print(a, a+5, ',');
+    co(vi.begin(), vi.end(), b);
+    print(b, b + 8);                //2 3 6 8 9 0 0 0
+    //co(b, b+5, b+3);              //+2 3 6 2 3 6 2 3
+    copy_backward(b, b+5, b+8);     //倒着复制
+    print(b, b + 8);
+
+}
