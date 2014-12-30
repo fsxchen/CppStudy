56de95902afe3d0aad04c5b535c7940069fd3dc7
fsxchen <fsxchen@gmail.com>
2014-12-30 16:03:02 +0800

    day02

---
+++ LinuxC/day02/map.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <sys/mman.h>
+#include <stdlib.h>
+#include <stdio.h>
+main() {
+    int *p=mmap(
+        NULL,      //NULL
+        getpagesize(),      //得到返回内存分配的基本单位
+        PROT_READ|PROT_WRITE,
+        MAP_ANONYMOUS|MAP_SHARED,
+        0, 0);
+    *p = 20;
+    *(p+1) = 30;
+    *(p+2) = 40;
+    printf("%d\n", p[2]);
+}
