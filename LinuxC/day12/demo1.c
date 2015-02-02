d87a168328ee9a298f601ddf60053ed833c90b22
fsxchen <fsxchen@gmail.com>
2015-02-02 23:24:59 +0800

    fsxchen

--- LinuxC/day12/demo1.c
+++ LinuxC/day12/demo1.c
@@ -8,6 +8,7 @@
 // 全局变量两个窗体
 WINDOW *wtime, *wnumb;
 pthread_t thnumb, thtime;
+pthread_mutex_t m;
 // 线程1：随机数
 //     循环产生7位随机数
 //     显示
@@ -17,10 +18,13 @@ void *runnumb(void *d) {
     int num;
     while(1) {
         num = rand() %10000000;
+        pthread_mutex_lock(&m);
         mvwprintw(wnumb, 1, 2, "%07d", num);
         refresh();
         wrefresh(wnumb);
-        usleep(10000);
+        pthread_mutex_unlock(&m);
+
+        usleep(100000);
     }
     return 0;
 }
@@ -36,10 +40,13 @@ void *runtime(void *d) {
     while(1) {
         tt = time(0);
         t = localtime(&tt);
+        pthread_mutex_lock(&m);
         mvwprintw(wtime, 1, 1, "%02d:%02d:%02d",
             t->tm_hour, t->tm_min, t->tm_sec);
         refresh();
         wrefresh(wtime);
+        pthread_mutex_unlock(&m);
+
         sleep(1);
     }
     return 0;
@@ -58,6 +65,7 @@ main() {
     refresh();
     wrefresh(wnumb);
     wrefresh(wtime);
+    pthread_mutex_init(&m, 0);
     // 创建线程1
     pthread_create(&thnumb, 0, runnumb, 0);
     // 创建线程2
@@ -66,6 +74,7 @@ main() {
     getch();
 
     // 结束
+    pthread_mutex_destroy(&m);
     delwin(wnumb);
     delwin(wtime);
     endwin();
---
+++ LinuxC/day12/problem2.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+pthread_mutex_t m;
+void handle(void *d) {
+    pthread_mutex_unlock(&m);
+    printf("调用!!!!\n");
+}
+
+void* runodd(void *d) {
+    int i;
+    for(i = 1;; i+=2) {
+        pthread_cleanup_push(handle, 0);    //在锁定期间调用pthread_exit()/pthread_cancel()以及
+                                            //+任何期间pthread_cleanup_pop参数为1都会触发这个函数回调
+        pthread_mutex_lock(&m);
+        // sleep(1);
+
+        printf("%d\n", i);
+        // if(i > 100000)
+        //     pthread_exit(0);     //另外一个会死锁
+        pthread_mutex_unlock(&m);
+        pthread_cleanup_pop(1);
+    }
+}
+
+void* runeven(void *d) {
+    int i;
+    for(i = 0;; i+=2) {
+        pthread_cleanup_push(handle, 0);
+        pthread_mutex_lock(&m);
+        // sleep(1);
+        printf("%d\n", i);
+        pthread_mutex_unlock(&m);
+        pthread_cleanup_pop(0);
+
+    }
+}
+
+main() {
+    pthread_t todd, tevent;
+    pthread_mutex_init(&m, 0);
+    pthread_create(&todd, 0, runodd, 0);
+    pthread_create(&tevent, 0, runeven, 0);
+    sleep(5);
+    pthread_cancel(todd);
+    pthread_join(todd, (void**)0);
+    pthread_join(tevent, (void**)0);
+    pthread_mutex_destroy(&m);
+
+}
